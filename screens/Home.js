99407a01c93a9b7df2d24c43a182a84a903d6f0b
Jatinder Singh <jatinder@Jatinders-MacBook-Pro.local>
2021-09-29 16:43:57 +0100

    assets added

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
+import Catagories from "../components/Catagories";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "./SearchBar";
 
@@ -10,6 +11,7 @@ export default function Home() {
         <HeaderTabs />
         <SearchBar />
       </View>
+      <Catagories />
     </SafeAreaView>
   );
 }
