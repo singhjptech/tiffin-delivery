1fbdb48ed8afed155b899a03b1314c22a6a77db4
Jatinder Singh <jatinder@Jatinders-MacBook-Pro.local>
2021-10-04 16:34:49 +0100

    yelpApi added

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, SafeAreaView, ScrollView } from "react-native";
 import Catagories from "../components/Catagories";
 import HeaderTabs from "../components/HeaderTabs";
-import RestaurantItems from "../components/RestaurantItems";
+import RestaurantItems, {
+  localRestaurants,
+} from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
+import { yelpApiKey } from "../apiKeys";
+
+const YELP_API_Key = yelpApiKey;
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=Manchester`;
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_Key}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => setRestaurantData(json.businesses));
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, []);
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
@@ -14,7 +39,7 @@ export default function Home() {
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Catagories />
-        <RestaurantItems />
+        <RestaurantItems restaurantData={restaurantData} />
       </ScrollView>
     </SafeAreaView>
   );
